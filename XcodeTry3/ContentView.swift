5f9d08384f2bab2702407fa16c07286f34fda2fe
CristianCaro915 <c.caro@uniandes.edu.co>
2024-09-04 22:32:33 -0500

    Initial commit

---
+++ XcodeTry3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeTry3
+//
+//  Created by Cristian Caro on 4/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeTry3/XcodeTry3App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTry3App.swift
+//  XcodeTry3
+//
+//  Created by Cristian Caro on 4/09/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTry3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
